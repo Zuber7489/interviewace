bd4d9effbf5f9495c1234d7970774bfbc68cf2cb
Zuber7489 <zs808081@gmail.com>
2026-02-26 17:04:09 +0530

    update

---
+++ src/services/toast.service.ts
@@ -0,0 +1,41 @@
+import { Injectable, signal } from '@angular/core';
+
+export type ToastType = 'success' | 'error' | 'info';
+
+export interface Toast {
+    id: string;
+    message: string;
+    type: ToastType;
+}
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ToastService {
+    toasts = signal<Toast[]>([]);
+
+    show(message: string, type: ToastType = 'info') {
+        const id = Math.random().toString(36).substring(2, 9);
+        this.toasts.update(current => [...current, { id, message, type }]);
+
+        setTimeout(() => {
+            this.remove(id);
+        }, 3000);
+    }
+
+    success(message: string) {
+        this.show(message, 'success');
+    }
+
+    error(message: string) {
+        this.show(message, 'error');
+    }
+
+    info(message: string) {
+        this.show(message, 'info');
+    }
+
+    remove(id: string) {
+        this.toasts.update(current => current.filter(t => t.id !== id));
+    }
+}
