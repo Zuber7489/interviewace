332dde0b031a087ee646b39658b2316cf514ca97
Zuber7489 <zs808081@gmail.com>
2026-03-06 20:31:09 +0530

    s

--- fix-env.js
+++
@@ -1,6 +0,0 @@
-import fs from 'fs';
-
-const content = `API_KEY=AIzaSyAkK91nWCPJCLpKlzvffcqGno3_HDb6N1I`;
-
-fs.writeFileSync('.env', content, { encoding: 'utf8' });
-console.log('.env file rewritten successfully');
